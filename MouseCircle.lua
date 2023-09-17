1bdef53394260e6068e91ce8b0502a8970804620
Fredrik <grahmfredrik@gmail.com>
2023-09-17 20:40:01 +0200

    reseting

--- MouseCircle.lua
+++ MouseCircle.lua
@@ -2,6 +2,8 @@
 DEFAULT_CHAT_FRAME:AddMessage("MouseCircle Loaded!")
 DEFAULT_CHAT_FRAME:AddMessage("To open settings panel enter /sm settings")
 
+settings = {}
+MouseCircleSettings = MouseCircleSettings or {}
 -- Create the frame
 local circleFrame = CreateFrame("Frame", nil, UIParent)
 circleFrame:SetSize(64, 64) -- Set the size of the frame
@@ -13,14 +15,12 @@ circleTexture:SetAllPoints(circleFrame)
 
 -- Create the settings panel frame
 local settingsPanel = CreateFrame("Frame", "MyAddonSettingsPanel", UIParent, "BackdropTemplate")
-settingsPanel:SetSize(300, 200)
+settingsPanel:SetSize(400, 300)
 settingsPanel:SetPoint("CENTER")
 
 settingsPanel.title = settingsPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 settingsPanel.title:SetPoint("TOP", 0, -10)
 settingsPanel.title:SetText("MouseCircle settings")
-
-
 
 -- Apply backdrop properties using the Backdrop mixin
 settingsPanel:SetBackdrop({
@@ -93,7 +93,7 @@ visibilityLabel:SetText("Show Circle")
 -- Add a slider to control the transparency of the circle
 local transparencySlider = CreateFrame("Slider", nil, settingsPanel, "OptionsSliderTemplate")
 transparencySlider:SetSize(200, 20)
-transparencySlider:SetPoint("TOPLEFT", visibilityCheckbox, "BOTTOMLEFT", 0, -10)
+transparencySlider:SetPoint("TOPLEFT", visibilityCheckbox, "BOTTOMLEFT", 0, -20)
 transparencySlider:SetMinMaxValues(0, 1)
 transparencySlider:SetValue(1)
 transparencySlider:SetValueStep(0.1)
@@ -107,27 +107,120 @@ end)
 local transparencyLabel = settingsPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 transparencyLabel:SetPoint("BOTTOMLEFT", transparencySlider, "TOPLEFT", 0, 10)
 transparencyLabel:SetText("Circle Transparency")
--- Add a button to reset the circle to default values
-local resetButton = CreateFrame("Button", nil, settingsPanel, "UIPanelButtonTemplate")
-resetButton:SetSize(100, 30)
-resetButton:SetPoint("TOPLEFT", 10, -150)
-resetButton:SetText("Reset Circle")
-resetButton:SetScript("OnClick", function()
-    sizeSlider:SetValue(64)
-    visibilityCheckbox:SetChecked(true)
-    transparencySlider:SetValue(1)
+
+
+
+
+
+
+
+-- Reset the size of the circle to its default value
+local resetSizeButton = CreateFrame("Button", nil, settingsPanel, "UIPanelButtonTemplate")
+resetSizeButton:SetSize(100, 30)
+resetSizeButton:SetPoint("LEFT", sizeSlider, "RIGHT", 10, 0)
+resetSizeButton:SetText("Default")
+resetSizeButton:SetScript("OnClick", function()
+    sizeSlider:SetValue(defaultSettings.circleSize)
+    settings.circleSize = defaultSettings.circleSize
 end)
 
--- Slash command handler
-local function SlashCommandHandler(msg)
-    if msg == "settings" then
-        if settingsPanel:IsShown() then
-            settingsPanel:Hide()
-        else
-            settingsPanel:Show()
-        end
-    end
+-- Reset the transparency of the circle to its default value
+local resetTransparencyButton = CreateFrame("Button", nil, settingsPanel, "UIPanelButtonTemplate")
+resetTransparencyButton:SetSize(100, 30)
+resetTransparencyButton:SetPoint("LEFT", transparencySlider, "RIGHT", 10, 0)
+resetTransparencyButton:SetText("Default")
+resetTransparencyButton:SetScript("OnClick", function()
+    transparencySlider:SetValue(defaultSettings.circleTransparency)
+    settings.circleTransparency = defaultSettings.circleTransparency
+end)
+
+-- Create the revert button (without the OnClick script)
+local revertButton = CreateFrame("Button", nil, settingsPanel, "UIPanelButtonTemplate")
+revertButton:SetSize(100, 30)
+revertButton:SetPoint("LEFT", saveButton, "RIGHT", 10, 0)
+revertButton:SetText("Revert")
+revertButton:Disable() -- Disable the button initially
+
+-- Create the save button
+local saveButton = CreateFrame("Button", nil, settingsPanel, "UIPanelButtonTemplate")
+saveButton:SetSize(100, 30)
+saveButton:SetPoint("BOTTOMLEFT", 10, 10)
+saveButton:SetText("Save")
+saveButton:Disable() -- Disable the button initially
+
+saveButton:SetScript("OnClick", function()
+    -- Save the current settings
+    settings.circleSize = sizeSlider:GetValue()
+    settings.circleVisibility = visibilityCheckbox:GetChecked()
+    settings.circleTransparency = transparencySlider:GetValue()
+
+    -- Disable the save and revert buttons since nothing has changed
+    saveButton:Disable()
+    revertButton:Disable()
+end)
+
+-- Create the revert button
+local revertButton = CreateFrame("Button", nil, settingsPanel, "UIPanelButtonTemplate")
+revertButton:SetSize(100, 30)
+revertButton:SetPoint("LEFT", saveButton, "RIGHT", 10, 0)
+revertButton:SetText("Revert")
+revertButton:Disable() -- Disable the button initially
+
+revertButton:SetScript("OnClick", function()
+    -- Revert the settings to the last saved state
+    sizeSlider:SetValue(settings.circleSize)
+    visibilityCheckbox:SetChecked(settings.circleVisibility)
+    transparencySlider:SetValue(settings.circleTransparency)
+
+    -- Disable the save and revert buttons since nothing has changed
+    saveButton:Disable()
+    revertButton:Disable()
+end)
+
+-- Enable the save and revert buttons when a setting is changed
+local function OnSettingChanged()
+    saveButton:Enable()
+    revertButton:Enable()
 end
+
+-- Modify the scripts to update the settings and call OnSettingChanged
+sizeSlider:SetScript("OnValueChanged", function(self, value)
+    local size = math.floor(value)
+    circleFrame:SetSize(size, size)
+    settings.circleSize = size -- Update the setting
+    OnSettingChanged()
+end)
+
+visibilityCheckbox:SetScript("OnClick", function(self)
+    local isChecked = self:GetChecked()
+    if isChecked then
+        circleFrame:Show()
+    else
+        circleFrame:Hide()
+    end
+    settings.circleVisibility = isChecked -- Update the setting
+    OnSettingChanged()
+end)
+
+transparencySlider:SetScript("OnValueChanged", function(self, value)
+    local alpha = value
+    circleTexture:SetAlpha(alpha)
+    settings.circleTransparency = alpha -- Update the setting
+    OnSettingChanged()
+end)
+
+local function SaveSettings()
+    MouseCircleSettings = settings -- Save the settings
+end
+
+circleFrame:RegisterEvent("PLAYER_LOGOUT")
+circleFrame:SetScript("OnEvent", function(self, event)
+    if event == "PLAYER_LOGOUT" then
+        SaveSettings()
+    else
+        SetCircleColorByClass()
+    end
+end)
 
 -- Register slash command
 SLASH_MOUSECIRCLE1 = "/sm"
@@ -169,6 +262,42 @@ end
 
 -- Register events and update circle position
 circleFrame:SetScript("OnUpdate", UpdateCirclePosition)
-circleFrame:RegisterEvent("PLAYER_LOGIN")
+
+
 circleFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-circleFrame:SetScript("OnEvent", SetCircleColorByClass)
+circleFrame:RegisterEvent("ADDON_LOADED")
+circleFrame:RegisterEvent("PLAYER_LOGOUT")
+
+
+local function SaveSettings()
+    MouseCircleSettings = settings -- Save the settings
+end
+
+
+-- Define default settings
+local defaultSettings = {
+    circleSize = 64,
+    circleVisibility = true,
+    circleTransparency = 1
+}
+circleFrame:SetScript("OnEvent", function(self, event, addonName)
+    if event == "ADDON_LOADED" and addonName == "MouseCircle" then
+        -- Load the saved settings or use the default settings if none exist
+        if MouseCircleSettings and MouseCircleSettings.circleSize then
+            print("Loaded custom MouseCircleSettings")
+        else
+            print("Loaded defaultSettings")
+            MouseCircleSettings = defaultSettings
+        end
+        settings = MouseCircleSettings
+
+        -- Apply the loaded settings
+        sizeSlider:SetValue(settings.circleSize or defaultSettings.circleSize)
+        visibilityCheckbox:SetChecked(settings.circleVisibility or defaultSettings.circleVisibility)
+        transparencySlider:SetValue(settings.circleTransparency or defaultSettings.circleTransparency)
+    elseif event == "PLAYER_LOGOUT" then
+        SaveSettings() -- Save the current settings
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        SetCircleColorByClass()
+    end
+end)
